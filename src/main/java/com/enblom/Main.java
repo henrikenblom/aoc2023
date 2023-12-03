c73f0f93fcc4667f33126bb3c4ba4e07d1a3961f
Henrik Enblom <henblom@spotify.com>
2023-12-03 19:54:08 +0100

    Unbloat Utils by moving all runner functionality to the Main class ❄️

--- src/main/java/com/enblom/Main.java
+++ src/main/java/com/enblom/Main.java
@@ -1,12 +1,17 @@
 package com.enblom;
 
-import static com.enblom.utils.Utils.getInput;
-
 import com.enblom.solvers.Solver;
 import com.enblom.utils.Utils;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.Random;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -56,7 +61,7 @@ public class Main {
     };
   }
 
-  public static Set<String> findSolverClassNames() {
+  private static Set<String> findSolverClassNames() {
     InputStream stream =
         ClassLoader.getSystemClassLoader().getResourceAsStream("com/enblom/solvers");
     assert stream != null;
@@ -65,5 +70,34 @@ public class Main {
         .lines()
         .filter(line -> line.endsWith("Solver.class") && line.startsWith("Day"))
         .collect(Collectors.toSet());
+  }
+
+  private static String getInput(int day) throws IOException {
+    final var inputPath =
+        Paths.get(System.getProperty("user.dir"), "input", "day%s/input.txt".formatted(day));
+    if (!Files.exists(inputPath)) {
+      downloadInput(day, inputPath);
+    }
+    return Files.readString(inputPath);
+  }
+
+  private static void downloadInput(int day, Path destination) throws IOException {
+    HttpURLConnection connection =
+        (HttpURLConnection)
+            new URL("https://adventofcode.com/2023/day/%s/input".formatted(day)).openConnection();
+    connection.setRequestMethod("GET");
+    connection.addRequestProperty("Cookie", "session=" + getSessionCookie());
+    Files.copy(connection.getInputStream(), destination, StandardCopyOption.REPLACE_EXISTING);
+  }
+
+  private static String getSessionCookie() {
+    final var sessionCookePath = Paths.get(System.getProperty("user.dir"), ".session_cookie");
+    try {
+      return Files.readAllLines(sessionCookePath).get(0);
+    } catch (Exception e) {
+      throw new RuntimeException(
+          "Could not read the session cookie. Save the AoC session cookie to %s, after logging in"
+              .formatted(sessionCookePath));
+    }
   }
 }
--- src/main/java/com/enblom/utils/Utils.java
+++ src/main/java/com/enblom/utils/Utils.java
@@ -1,12 +1,5 @@
 package com.enblom.utils;
 
-import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,35 +15,6 @@ public class Utils {
 
   public static int grabIntegers(String input) {
     return Integer.parseInt(extractDigits(input));
-  }
-
-  public static String getInput(int day) throws IOException {
-    final var inputPath =
-        Paths.get(System.getProperty("user.dir"), "input", "day%s/input.txt".formatted(day));
-    if (!Files.exists(inputPath)) {
-      downloadInput(day, inputPath);
-    }
-    return Files.readString(inputPath);
-  }
-
-  public static void downloadInput(int day, Path destination) throws IOException {
-    HttpURLConnection connection =
-        (HttpURLConnection)
-            new URL("https://adventofcode.com/2023/day/%s/input".formatted(day)).openConnection();
-    connection.setRequestMethod("GET");
-    connection.addRequestProperty("Cookie", "session=" + getSessionCookie());
-    Files.copy(connection.getInputStream(), destination, StandardCopyOption.REPLACE_EXISTING);
-  }
-
-  private static String getSessionCookie() {
-    final var sessionCookePath = Paths.get(System.getProperty("user.dir"), ".session_cookie");
-    try {
-      return Files.readAllLines(sessionCookePath).get(0);
-    } catch (Exception e) {
-      throw new RuntimeException(
-          "Could not read the session cookie. Save the AoC session cookie to %s, after logging in"
-              .formatted(sessionCookePath));
-    }
   }
 
   public static List<Integer> findAllIntegers(String input) {
