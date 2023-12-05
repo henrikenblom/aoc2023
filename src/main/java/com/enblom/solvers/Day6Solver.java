16b1ca8e3833544cbd30bd22f1b6672c0c275d95
Henrik Enblom <henblom@spotify.com>
2023-12-05 20:59:00 +0100

    Prepare for day 6 🎁

---
+++ src/main/java/com/enblom/solvers/Day6Solver.java
@@ -0,0 +1,18 @@
+package com.enblom.solvers;
+
+public class Day6Solver extends Solver {
+
+  public Day6Solver(String input) {
+    super(input);
+  }
+
+  @Override
+  public Long solveFirstPuzzle() {
+    return null;
+  }
+
+  @Override
+  public Long solveSecondPuzzle() {
+    return null;
+  }
+}
---
+++ src/test/java/com/enblom/solvers/Day6SolverTest.java
@@ -0,0 +1,16 @@
+package com.enblom.solvers;
+
+class Day6SolverTest extends SolverTestBase<Day6Solver> {
+
+  @Override
+  void solvesFirst() {
+    solvesFirst("""
+        """, 0);
+  }
+
+  @Override
+  void solvesSecond() {
+    solvesSecond("""
+        """, 0);
+  }
+}
