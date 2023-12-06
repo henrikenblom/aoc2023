854c11548b2b32aa09d94bb3dfbc47bf1bcefc2d
Henrik Enblom <henblom@spotify.com>
2023-12-06 08:24:42 +0100

    Prepare a bit more for day 7 🎄

--- src/test/java/com/enblom/solvers/Day7SolverTest.java
+++ src/test/java/com/enblom/solvers/Day7SolverTest.java
@@ -3,8 +3,14 @@ package com.enblom.solvers;
 class Day7SolverTest extends SolverTestBase<Day7Solver> {
 
   @Override
-  void solvesFirst() {}
+  void solvesFirst() {
+    solvesFirst("""
+        """, 0);
+  }
 
   @Override
-  void solvesSecond() {}
+  void solvesSecond() {
+    solvesSecond("""
+        """, 0);
+  }
 }
